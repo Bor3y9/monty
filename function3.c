8986539cd0f19783aed89acb7b8fd42341f78364
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-10-18 23:19:06 +0300

    mul function

--- function3.c
+++ function3.c
@@ -21,3 +21,32 @@ void mul(stack_t **stack, unsigned int line_counter)
     pop(stack, line_counter);
     (*stack)->n = mul;
 }
+/**
+ * mod - computes the rest of the division of the second top element
+ * of the stack by the top element of the stack
+ * @stack: pointer to the head of the stack
+ * @line_counter: line number of the opcode
+ * Return: void
+ */
+void mod(stack_t **stack, unsigned int line_counter)
+{
+    stack_t *tmp = *stack;
+    int mod;
+    (void)line_counter;
+
+    if (tmp == NULL || tmp->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't mod, stack too short\n", line_counter);
+        free_all(*stack, state.arg);
+        exit(EXIT_FAILURE);
+    }
+    if (tmp->n == 0)
+    {
+        fprintf(stderr, "L%d: division by zero\n", line_counter);
+        free_all(*stack, state.arg);
+        exit(EXIT_FAILURE);
+    }
+    mod = (tmp->next)->n % tmp->n;
+    pop(stack, line_counter);
+    (*stack)->n = mod;
+}
