9fb16580679a027638afa856af26969a0914f7a9
Bor3y9 <yousef161247@eng.psu.edu.eg>
2023-10-18 22:58:56 +0300

    hot fixes

--- function2.c
+++ function2.c
@@ -33,3 +33,27 @@ void nop(stack_t **stack, unsigned int line_counter)
 	(void)stack;
 	(void)line_counter;
 }
+/**
+ * sub - subtracts the top element of the stack from the second top element
+ * @stack: pointer to the head of the stack
+ * @line_counter: line number of the opcode
+ * Return: void
+ */
+void sub(stack_t **stack, unsigned int line_counter)
+{
+    stack_t *tmp = *stack;
+    int sub;
+    (void)line_counter;
+
+    if (tmp == NULL || tmp->next == NULL)
+    {
+        fprintf(stderr, "L%d: can't sub, stack too short\n", line_counter);
+        free_all(*stack, state.arg);
+        exit(EXIT_FAILURE);
+    }
+    sub = (tmp->next)->n - tmp->n;
+    pop(stack, line_counter);
+    (*stack)->n = sub;
+}
+
+
